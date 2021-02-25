be079ba28be61e0b5e63982bfb2635c14ebc92c1
KcRaymond <kaseyleigh1978@gmail.com>
2021-02-25 13:57:14 -0700

    updated server.js code

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
+//handles server requests
 const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = 3000;
+const logger = require("morgan");
+//middleware logger logs details
+const mongoose = require("mongoose");
+//implements JS code to work with MongoDB
+const compression = require("compression");
+//compresses data that is served to users
+
+const PORT = process.env.PORT || 3000;
+//sets env environment or local host port 3000
 
 const app = express();
+//sets express connection to app
 
 app.use(logger("dev"));
+//Morgan connection
 
 app.use(compression());
+//compression connection
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//express app connections
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//mongoose connection that uses Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budgetTracker",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
